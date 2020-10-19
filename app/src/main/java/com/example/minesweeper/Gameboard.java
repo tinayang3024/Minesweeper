cf64a0bad971fc8e4cb1a4c82af29caba3105303
Tina Yang <tinayang3024@gmail.com>
2020-10-18 20:47:40 -0400

    add node and gameboard classes

---
+++ app/src/main/java/com/example/minesweeper/Gameboard.java
@@ -0,0 +1,7 @@
+package com.example.minesweeper;
+
+public class Gameboard {
+    int dimension = 0;
+    int num_of_mines = 0;
+    int[][] data = new int[0][0];
+}
---
+++ app/src/main/java/com/example/minesweeper/Node.java
@@ -0,0 +1,6 @@
+package com.example.minesweeper;
+
+public class Node {
+    int surrounding_mines = 0;
+    boolean is_mine = false;
+}
