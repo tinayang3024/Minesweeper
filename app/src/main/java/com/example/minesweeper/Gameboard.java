c239916ef72133a39d2c83f16b3bf0c37fae9a7b
Sophie Mao <shsm715@gmail.com>
2020-10-18 21:34:10 -0400

    Add basic functions to Gameboard, add flagged member to Node

--- app/src/main/java/com/example/minesweeper/Gameboard.java
+++ app/src/main/java/com/example/minesweeper/Gameboard.java
@@ -1,7 +1,29 @@
 package com.example.minesweeper;
 
+import java.util.Random;
+//import java.util.ArrayList;
+
 public class Gameboard {
-    int dimension = 0;
-    int num_of_mines = 0;
-    int[][] data = new int[0][0];
+    int row = 9;
+    int col = 9;
+    int num_mines = 10;
+    int[][] data = new Node[0][0];
+
+    public void set_row(
+            int target_row
+    ) {
+        row = target_row;
+    }
+    public void set_col(
+            int target_col
+    ) {
+        col = target_col;
+    }
+    public void set_num_mines(
+            int target_num_mines
+    ) {
+        num_mines = target_num_mines;
+    }
+
+
 }
--- app/src/main/java/com/example/minesweeper/Node.java
+++ app/src/main/java/com/example/minesweeper/Node.java
@@ -3,4 +3,5 @@ package com.example.minesweeper;
 public class Node {
     int surrounding_mines = 0;
     boolean is_mine = false;
+    boolean flagged = false;
 }
